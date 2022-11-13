384e1dffb3823e17a84c97eb0463d074b78aea17
Ivan M <slade91@mail.ru>
2022-11-13 20:10:01 +0300

    added Program.cs

---
+++ Program.cs
@@ -0,0 +1,54 @@
+﻿/*Задача 62. Напишите программу, которая заполнит спирально массив 4 на 4.
+Например, на выходе получается вот такой массив:
+01 02 03 04
+12 13 14 05
+11 16 15 06
+10 09 08 07*/
+
+
+void PrintArray(int[,] arr)
+{
+    Console.WriteLine("Заполненный спирально массив 4 на 4 имеет следующий вид:");
+    for (int i = 0; i < arr.GetLength(0); i++)
+    {
+        for (int j = 0; j < arr.GetLength(1); j++)
+        {
+            if (arr[i, j] / 10 <= 0)
+                Console.Write($" {arr[i, j]} ");
+            else Console.Write($"{arr[i, j]} ");
+        }
+        Console.WriteLine();
+    }
+
+
+}
+
+void SpiralElements(int[,] arr)
+{
+    int temp = 1;
+    int i = 0;
+    int j = 0;
+
+    while (temp <= arr.GetLength(0) * arr.GetLength(1))
+    {
+        arr[i, j] = temp;
+        temp++;
+        if (i <= j + 1 && i + j < arr.GetLength(1) - 1)
+            j++;
+        else if (i < j && i + j >= arr.GetLength(0) - 1)
+            i++;
+        else if (i >= j && i + j > arr.GetLength(1) - 1)
+            j--;
+        else
+            i--;
+    }
+
+    PrintArray(arr);
+
+}
+
+
+int line = 4;
+int column = 4;
+int[,] arr = new int[line, column];
+SpiralElements(arr);
